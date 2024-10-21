e4b6eccc88a619d24d33658c85023d302d74ce69
DmytroHlazyrin <dmytro.hlazyrin@gmail.com>
2024-10-21 14:31:16 +0300

    Change routers in main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
-from app.routers import books, authors, genres, publishers, borrowers, borrowing_history
+from app.routers import (
+    books,
+    authors,
+    genres,
+    publishers,
+    auth,
+    borrowings,
+    users,
+)
 from db.engine import engine
 from db.models import Base
 
@@ -13,16 +21,18 @@ app = FastAPI()
 # Include Routers
 # ------------------------------------
 
+app.include_router(auth.router)
+app.include_router(users.router)
 app.include_router(books.router)
 app.include_router(authors.router)
 app.include_router(genres.router)
 app.include_router(publishers.router)
-app.include_router(borrowers.router)
-app.include_router(borrowing_history.router)
+app.include_router(borrowings.router)
 
 # ------------------------------------
 # Root Endpoint
 # ------------------------------------
+
 
 @app.get("/")
 async def read_root() -> dict:
