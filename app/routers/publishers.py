2c595cb5065f3d926d35b6ecf56452468cdceb28
DmytroHlazyrin <dmytro.hlazyrin@gmail.com>
2024-10-21 14:28:49 +0300

    Added routers for Publishers

--- app/routers/publishers.py
+++ app/routers/publishers.py
@@ -1,8 +1,11 @@
+from typing import Literal
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas import Publisher, PublisherCreate
 from app.crud import create_publisher, get_publisher, get_publishers
-from app.dependencies import get_db
+from app.dependencies import get_db, admin_required
+from db import models
 
 router = APIRouter()
 
@@ -10,19 +13,34 @@ router = APIRouter()
 # Endpoints for Publishers
 # ------------------------------------
 
+
 @router.post("/publishers/", response_model=Publisher)
 async def create_publisher_endpoint(
-    publisher: PublisherCreate, db: Session = Depends(get_db)
+    publisher: PublisherCreate,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(admin_required),
 ) -> Publisher:
     """Create a new publisher."""
     return create_publisher(db=db, publisher=publisher)
 
+
 @router.get("/publishers/", response_model=list[Publisher])
 async def get_publishers_endpoint(
-    skip: int = 0, limit: int = 10, db: Session = Depends(get_db)
+    offset: int = 0,
+    limit: int = 10,
+    sort_by: str = "name",
+    sort_order: Literal["asc", "desc"] = "asc",
+    db: Session = Depends(get_db),
 ) -> list[Publisher]:
     """Retrieve a list of publishers with pagination."""
-    return get_publishers(db=db, skip=skip, limit=limit)
+    return get_publishers(
+        db=db,
+        offset=offset,
+        limit=limit,
+        sort_by=sort_by,
+        sort_order=sort_order,
+    )
+
 
 @router.get("/publishers/{publisher_id}", response_model=Publisher)
 async def get_publisher_endpoint(
